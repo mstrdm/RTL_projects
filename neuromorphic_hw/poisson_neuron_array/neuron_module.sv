7b6de713e230e013224cb1a5e553af45c617b9f1
mstrdm <k_vladimir@naver.com>
2019-08-02 16:28:55 +0900

    Updated Neuron Module

--- neuromorphic_hw/poisson_neuron_array/neuron_module.sv
+++ neuromorphic_hw/poisson_neuron_array/neuron_module.sv
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
-module neuron_module #(parameter NEURON_NUMBER=256, ACTIVITY_WIDTH=9, REFRACTORY_WIDTH=4, REFRACTORY_PER=4)
+module neuron_module #(parameter NEURON_NUMBER=256, ACTIVITY_WIDTH=9, REFRACTORY_WIDTH=4, REFRACTORY_PER=4, TS_WIDTH=16)
 // ACTIVITY_WIDTH 		- number of bits for encoding activity
 // REFRACTORY_WIDTH		- number of bits for encoding refractory state
-// REFRACTORY_PER - refractory period measured in dT
+// REFRACTORY_PER 		- refractory period measured in dT
 	(input logic clk, reset,
 	 //external access signals
 	 input logic ext_req,													// external memory access request
@@ -11,13 +11,14 @@ module neuron_module #(parameter NEURON_NUMBER=256, ACTIVITY_WIDTH=9, REFRACTORY
 	 input logic [$clog2(NEURON_NUMBER)-1:0] ext_neur_addr,					// external access address
 	 input logic [NEUR_WIDTH-1:0] ext_neur_data_in,
 	 output logic [NEUR_WIDTH-1:0] ext_neur_data_out,
-	 // 
 	 input logic sys_en,													// flag for enabling the system operation
-	 output logic module_busy												// indicates that external access will be ignored
+	 output logic module_busy,												// indicates that external access will be ignored
+	 output logic spike_out,
+	 output logic [(TS_WIDTH+$clog2(NEURON_NUMBER))-1:0] event_out						// time stamp + neuron address
 	 );
 
 localparam NEUR_WIDTH = ACTIVITY_WIDTH + REFRACTORY_WIDTH;					// number of bits per neuron
-localparam DT = 10000;														// number of clock cycles to wait for 1 ms
+localparam DT = 100000;														// number of clock cycles to wait for 1 ms
 
 // ----------------------------------------------------------------------------
 // Main neuron memory; external and internal access definitions
@@ -48,14 +49,21 @@ assign ext_neur_data_out = neur_data_out;
 // ----------------------------------------------------------------------------
 // dT counter (1 ms); responsible for system's time resolution
 // ----------------------------------------------------------------------------
-logic [$clog2(DT)-1:0] dt_count;
-logic dt_tick;
+logic [$clog2(DT)-1:0] dt_count, dt_count_free;
+logic dt_tick, dt_tick_free;
+// Comment: _free versions run independently of sys_en and are only used for time stamping
 
 always @(posedge clk)
 	if (reset|dt_tick) dt_count <= 0;
 	else if (sys_en) dt_count <= dt_count + 1;
 
 assign dt_tick = (dt_count == DT-1);
+
+always @(posedge clk)
+	if (reset|dt_tick_free) dt_count_free <= 0;
+	else dt_count_free <= dt_count_free + 1;
+
+assign dt_tick_free = (dt_count_free == DT-1);
 
 // ----------------------------------------------------------------------------
 // Memory scrolling counter;
@@ -64,7 +72,7 @@ assign dt_tick = (dt_count == DT-1);
 logic [$clog2(NEURON_NUMBER+1)-1:0] scroll_count;
 logic scroll_on;															// asserted when scrolling through the memory
 
-assign scroll_on = scroll_count < NEURON_NUMBER;
+assign scroll_on = (scroll_count < NEURON_NUMBER) & sys_en;
 
 always @(posedge clk)
 	if (reset) scroll_count <= NEURON_NUMBER;
@@ -85,7 +93,7 @@ always @(posedge clk)
 		int_neur_rd_addr_d <= 0;
 		int_neur_wr_addr <= 0;
 	end
-	else begin
+	else if (sys_en) begin
 		int_neur_rd_addr_d <= int_neur_rd_addr;
 		int_neur_wr_addr <= int_neur_rd_addr_d;							// write address is two clock cycles delayed from the read address (1 clk for reading from memory and 1 clk for poisson module processing)		
 	end
@@ -118,6 +126,22 @@ assign module_busy = (int_re|int_we) & sys_en;							// prevent external access 
 assign ext_access = ext_req & ~module_busy;								
 assign ext_ack = ext_access;
 
+// ----------------------------------------------------------------------------
+// Time stamping output data
+// ----------------------------------------------------------------------------
+logic [TS_WIDTH-1:0] time_stamp;
+
+always @(posedge clk)
+	if (reset) time_stamp <= 0;
+	else if (dt_tick_free) time_stamp <= time_stamp + 1'b1;
+
+always @(posedge clk)
+	if (reset) spike_out <= 0;
+	else spike_out <= spike;
+
+always @(posedge clk)
+	if (reset) event_out <= 0;
+	else if (spike) event_out <= {time_stamp, int_neur_wr_addr};
 
 // ----------------------------------------------------------------------------
 // Initialization
@@ -135,6 +159,11 @@ initial begin
 		if (i==10) neur_ram[i] = {9'd511, 4'b0};
 		else neur_ram[i] = 0;
 	end
+
+	dt_count_free = 0;
+	time_stamp = 0;
+	spike_out = 0;
+	event_out = 0;
 
 end
 
